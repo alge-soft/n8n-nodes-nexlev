872d7b6ae9f1b29a3043f37b1fcbdf28989da2f9
Sohel Kabir <sohel.nkabir@gmail.com>
2025-11-14 11:05:03 +0600

    chore: update dist files for version 0.1.7

--- dist/nodes/Nexlev/config.d.ts
+++ dist/nodes/Nexlev/config.d.ts
@@ -1,4 +1,10 @@
 export interface Config {
     baseURL: string;
 }
+export declare enum Environment {
+    DEV = "dev",
+    PROD = "prod",
+    LOCAL = "local"
+}
+export declare const ENVIRONMENT = Environment.PROD;
 export declare const config: Config;
